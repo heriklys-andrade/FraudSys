4402c241792c81d1c028604be062c319b25e246c
herikiklys-andrade <heriklysalvaroaa@gmail.com>
2025-07-12 18:34:04 -0300

    Criação da entidade Cliente.

---
+++ FraudSys.Domain/Entities/ClientEntity.cs
@@ -0,0 +1,33 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace FraudSys.Domain.Entities
+{
+    [DynamoDBTable("clients")]
+    public class ClientEntity
+    {
+        public ClientEntity()
+        {
+
+        }
+
+        public ClientEntity(string documento, int agencia, string conta, double limitePix)
+        {
+            Documento = documento;
+            Agencia = agencia;
+            Conta = conta;
+            LimitePix = limitePix;
+        }
+
+        [DynamoDBHashKey("document")]
+        public string Documento { get; private set; }
+
+        [DynamoDBProperty("agency")]
+        public int Agencia { get; private set; }
+
+        [DynamoDBProperty("account")]
+        public string Conta { get; private set; }
+
+        [DynamoDBProperty("pix_limit")]
+        public double LimitePix { get; private set; }
+    }
+}
---
+++ FraudSys.Test/Domain/Entities/ClientEntityTest.cs
@@ -0,0 +1,31 @@
+﻿using Bogus;
+using FluentAssertions;
+using FraudSys.Domain.Entities;
+
+namespace FraudSys.Test.Domain.Entities
+{
+    public class ClientEntityTest
+    {
+        [Fact]
+        public void ClientEntityConstructor_Success()
+        {
+            //Arrange
+            string documento = "12345678901";
+            int agencia = 101;
+            string conta = "123-1";
+            double limitePix = 1000.00;
+            var expectedClient = new Faker<ClientEntity>()
+                .RuleFor(c => c.Documento, f => documento)
+                .RuleFor(c => c.Agencia, f => agencia)
+                .RuleFor(c => c.Conta, f => conta)
+                .RuleFor(c => c.LimitePix, f => limitePix)
+                .Generate();
+
+            //Act
+            var client = new ClientEntity(documento, agencia, conta, limitePix);
+
+            //Assert
+            client.Should().BeEquivalentTo(expectedClient);
+        }
+    }
+}
