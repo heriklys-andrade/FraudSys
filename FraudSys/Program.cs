c38b2ccdf1f3dac065612ffa84d89d532f5362df
herikiklys-andrade <heriklysalvaroaa@gmail.com>
2025-07-12 18:34:35 -0300

    Configuração inicial do DynamoDB.

--- FraudSys/Program.cs
+++ FraudSys/Program.cs
@@ -1,5 +1,12 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var awsOptions = builder.Configuration.GetAWSOptions();
+builder.Services.AddDefaultAWSOptions(awsOptions);
+builder.Services.AddAWSService<IAmazonDynamoDB>();
+builder.Services.AddScoped<IDynamoDBContext, DynamoDBContext>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
