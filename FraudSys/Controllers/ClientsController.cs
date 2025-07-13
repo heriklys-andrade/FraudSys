7ba81408c3296edeb228e21eecc42729b3fec831
herikiklys-andrade <heriklysalvaroaa@gmail.com>
2025-07-13 12:08:38 -0300

    Criação de controller e rota get.

---
+++ FraudSys/Controllers/ClientsController.cs
@@ -0,0 +1,31 @@
+﻿using FraudSys.Domain.Interfaces.Services;
+using FraudSys.Domain.Services.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FraudSys.Api.Controllers
+{
+    [Route("v1/[controller]")]
+    public class ClientsController(IClientService clientService) : Controller
+    {
+        [HttpGet()]
+        public async Task<IActionResult> GetClient(GetClientRequest request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                return Ok(await clientService.GetClientAsync(request, cancellationToken));
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return Problem(ex.Message);
+            }
+        }
+    }
+}
